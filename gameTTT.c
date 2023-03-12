599b75b44b914732f498150d3a33dfe1b9447be3
nwhator <nwhator@gmail.com>
2023-03-12 12:01:27 -0700

    Board output not displaying

--- gameTTT.c
+++ gameTTT.c
@@ -3,19 +3,23 @@
 /**
  * draw_board - Entry point
  * Description: Tic-Tic-Toe board
+ * @board: Char
  */
 
-void draw_board()
+void draw_board(char board[3][3])
 {
-	char board[3][3];
+	int i;
 
 	/* Default tic-tac-toe board*/
 	printf("\n");
-	printf("  %c   |  %c  |  %c\n", board[0][0], board[0][1], board[0][2]);
-	printf("-----+-----+-----\n");
-	printf("  %c   |  %c   |  %c\n", board[1][0], board[1][1], board[1][2]);
-	printf("-----+-----+-----\n");
-	printf("  %c   |  %c   |  %c\n", board[2][0], board[2][1], board[2][2]);
+	for (i = 0; i < 3; i++)
+	{
+		printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
+		if (i < 2)
+		{
+			printf("\n---|---|---\n");
+		}
+	}
 	printf("\n");
 }
 
